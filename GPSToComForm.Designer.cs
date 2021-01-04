22be5267ae5d9aeeab60dd0474e91c6161fdecfb
MattCW1701 <73855600+MattCW1701@users.noreply.github.com>
2021-01-03 23:37:46 -0500

    Add files via upload

---
+++ GPSToComForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace GPSD2Com
+{
+  partial class GPSToComForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.oTransmitPort = new System.IO.Ports.SerialPort(this.components);
+      this.cboSerialport = new System.Windows.Forms.ComboBox();
+      this.txtHost = new System.Windows.Forms.TextBox();
+      this.lblSerialPort = new System.Windows.Forms.Label();
+      this.lblHostName = new System.Windows.Forms.Label();
+      this.btnStart = new System.Windows.Forms.Button();
+      this.txtOutput = new System.Windows.Forms.TextBox();
+      this.btnStop = new System.Windows.Forms.Button();
+      this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+      this.lblPort = new System.Windows.Forms.Label();
+      this.txtPort = new System.Windows.Forms.TextBox();
+      this.chkLogging = new System.Windows.Forms.CheckBox();
+      this.SuspendLayout();
+      // 
+      // cboSerialport
+      // 
+      this.cboSerialport.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.cboSerialport.FormattingEnabled = true;
+      this.cboSerialport.Location = new System.Drawing.Point(112, 14);
+      this.cboSerialport.Name = "cboSerialport";
+      this.cboSerialport.Size = new System.Drawing.Size(253, 28);
+      this.cboSerialport.Sorted = true;
+      this.cboSerialport.TabIndex = 4;
+      // 
+      // txtHost
+      // 
+      this.txtHost.Location = new System.Drawing.Point(112, 48);
+      this.txtHost.Name = "txtHost";
+      this.txtHost.Size = new System.Drawing.Size(253, 26);
+      this.txtHost.TabIndex = 0;
+      // 
+      // lblSerialPort
+      // 
+      this.lblSerialPort.AutoSize = true;
+      this.lblSerialPort.Location = new System.Drawing.Point(24, 17);
+      this.lblSerialPort.Name = "lblSerialPort";
+      this.lblSerialPort.Size = new System.Drawing.Size(82, 20);
+      this.lblSerialPort.TabIndex = 2;
+      this.lblSerialPort.Text = "Serial Port";
+      this.lblSerialPort.TextAlign = System.Drawing.ContentAlignment.TopRight;
+      // 
+      // lblHostName
+      // 
+      this.lblHostName.AutoSize = true;
+      this.lblHostName.Location = new System.Drawing.Point(17, 51);
+      this.lblHostName.Name = "lblHostName";
+      this.lblHostName.Size = new System.Drawing.Size(89, 20);
+      this.lblHostName.TabIndex = 3;
+      this.lblHostName.Text = "Host Name";
+      this.lblHostName.TextAlign = System.Drawing.ContentAlignment.TopRight;
+      // 
+      // btnStart
+      // 
+      this.btnStart.Location = new System.Drawing.Point(112, 80);
+      this.btnStart.Name = "btnStart";
+      this.btnStart.Size = new System.Drawing.Size(75, 30);
+      this.btnStart.TabIndex = 2;
+      this.btnStart.Text = "Start";
+      this.btnStart.UseVisualStyleBackColor = true;
+      this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+      // 
+      // txtOutput
+      // 
+      this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.txtOutput.Location = new System.Drawing.Point(13, 117);
+      this.txtOutput.Multiline = true;
+      this.txtOutput.Name = "txtOutput";
+      this.txtOutput.ReadOnly = true;
+      this.txtOutput.Size = new System.Drawing.Size(546, 254);
+      this.txtOutput.TabIndex = 5;
+      // 
+      // btnStop
+      // 
+      this.btnStop.Enabled = false;
+      this.btnStop.Location = new System.Drawing.Point(194, 80);
+      this.btnStop.Name = "btnStop";
+      this.btnStop.Size = new System.Drawing.Size(75, 30);
+      this.btnStop.TabIndex = 3;
+      this.btnStop.Text = "Stop";
+      this.btnStop.UseVisualStyleBackColor = true;
+      this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+      // 
+      // lblPort
+      // 
+      this.lblPort.AutoSize = true;
+      this.lblPort.Location = new System.Drawing.Point(371, 51);
+      this.lblPort.Name = "lblPort";
+      this.lblPort.Size = new System.Drawing.Size(38, 20);
+      this.lblPort.TabIndex = 7;
+      this.lblPort.Text = "Port";
+      // 
+      // txtPort
+      // 
+      this.txtPort.Location = new System.Drawing.Point(416, 48);
+      this.txtPort.MaxLength = 6;
+      this.txtPort.Name = "txtPort";
+      this.txtPort.Size = new System.Drawing.Size(100, 26);
+      this.txtPort.TabIndex = 1;
+      this.txtPort.Text = "2947";
+      // 
+      // chkLogging
+      // 
+      this.chkLogging.AutoSize = true;
+      this.chkLogging.Location = new System.Drawing.Point(416, 18);
+      this.chkLogging.Name = "chkLogging";
+      this.chkLogging.Size = new System.Drawing.Size(92, 24);
+      this.chkLogging.TabIndex = 8;
+      this.chkLogging.Text = "Logging";
+      this.chkLogging.UseVisualStyleBackColor = true;
+      // 
+      // GPSToComForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(571, 383);
+      this.Controls.Add(this.chkLogging);
+      this.Controls.Add(this.txtPort);
+      this.Controls.Add(this.lblPort);
+      this.Controls.Add(this.btnStop);
+      this.Controls.Add(this.txtOutput);
+      this.Controls.Add(this.btnStart);
+      this.Controls.Add(this.lblHostName);
+      this.Controls.Add(this.lblSerialPort);
+      this.Controls.Add(this.txtHost);
+      this.Controls.Add(this.cboSerialport);
+      this.Name = "GPSToComForm";
+      this.Text = "GPSD2Com";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GPSToComForm_FormClosing);
+      this.Load += new System.EventHandler(this.GPSToComForm_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.IO.Ports.SerialPort oTransmitPort;
+    private System.Windows.Forms.ComboBox cboSerialport;
+    private System.Windows.Forms.TextBox txtHost;
+    private System.Windows.Forms.Label lblSerialPort;
+    private System.Windows.Forms.Label lblHostName;
+    private System.Windows.Forms.Button btnStart;
+    private System.Windows.Forms.TextBox txtOutput;
+    private System.Windows.Forms.Button btnStop;
+    private System.IO.Ports.SerialPort serialPort2;
+    private System.Windows.Forms.Label lblPort;
+    private System.Windows.Forms.TextBox txtPort;
+    private System.Windows.Forms.CheckBox chkLogging;
+  }
+}
+
---
+++ GPSToComForm.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GPSD2Com
+{
+  public partial class GPSToComForm : Form
+  {
+    Thread thTCPReadThread;
+    public delegate void UpdateTextbox(string sText);
+    public UpdateTextbox UpdateTextboxDelegate;
+    private volatile bool bTerminateTCPThread = false;
+    private volatile bool bLogging = false;
+
+    TcpClient tcpClient;
+    NetworkStream nsStream;
+    StreamWriter swWriter;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public GPSToComForm()
+    {
+      InitializeComponent();      
+      UpdateTextboxDelegate = new UpdateTextbox(UpdateTextboxMethod);
+    }
+
+    /// <summary>
+    /// Load method
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void GPSToComForm_Load(object sender, EventArgs e)
+    {
+      string[] sarPortNames = SerialPort.GetPortNames();
+      foreach (string sPortName in sarPortNames)
+      {
+        cboSerialport.Items.Add(sPortName);
+      }
+      cboSerialport.Text = sarPortNames[0];
+      txtHost.Text = GPSDirectToCom.Properties.Settings.Default.DefaultHostname;      
+    }
+
+    /// <summary>
+    /// Closing handler
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void GPSToComForm_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      Cleanup();
+      GPSDirectToCom.Properties.Settings.Default.DefaultHostname = txtHost.Text;
+      GPSDirectToCom.Properties.Settings.Default.Save();
+    }
+
+    /// <summary>
+    /// Start button handler
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void btnStart_Click(object sender, EventArgs e)
+    {
+      if(chkLogging.Checked)
+      {
+        swWriter = new StreamWriter($"GPSDToCom_{DateTime.Now.ToString("yyyyMMddHHmmss")}.log");
+        bLogging = true;
+      }
+      bTerminateTCPThread = false;
+      
+      if (SetupGPSDConnection())
+      {
+        SetupSerial();
+        EnableControls(true);
+        thTCPReadThread = new Thread(ReadTCPMethod);
+        thTCPReadThread.Start();
+      }        
+    }
+
+    /// <summary>
+    /// Stop button handler
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void btnStop_Click(object sender, EventArgs e)
+    {
+      Cleanup();
+      EnableControls(false);
+    }
+
+    /// <summary>
+    /// Method to update text box with the specified text
+    /// </summary>
+    /// <param name="sText"></param>
+    private void UpdateTextboxMethod(string sText)
+    {
+      txtOutput.Text = sText;
+    }
+
+    /// <summary>
+    /// Method to setup and open the serial port
+    /// </summary>
+    private void SetupSerial()
+    {
+      if (!string.IsNullOrWhiteSpace(cboSerialport.Text))
+      {
+        oTransmitPort.BaudRate = 4800;
+        oTransmitPort.PortName = cboSerialport.Text;
+        oTransmitPort.Open();
+      }
+    }
+
+    /// <summary>
+    /// Method to setup the TCP connection to GPSD and start the main listening thread
+    /// </summary>
+    private bool SetupGPSDConnection()
+    {
+      try
+      {
+        tcpClient = new TcpClient(txtHost.Text, int.Parse(txtPort.Text));
+      }
+      catch(Exception ex)
+      {
+        MessageBox.Show(ex.Message, "Error");
+        return false;
+      }
+      
+      nsStream = tcpClient.GetStream();
+      if (!tcpClient.Connected)
+        return false;
+      byte[] data = Encoding.ASCII.GetBytes("?WATCH={\"enable\":true,\"nmea\":true}");
+      nsStream.Write(data, 0, data.Length);
+      return true;
+    }
+
+    /// <summary>
+    /// Method to read from the TCP connectionss
+    /// </summary>
+    private void ReadTCPMethod()
+    {     
+      while(!bTerminateTCPThread)
+      {
+        byte[] yarData = new byte[256];
+        if (!nsStream.CanRead || !nsStream.DataAvailable)
+          continue;
+        int bytes = nsStream.Read(yarData, 0, yarData.Length);
+        string sRead = Encoding.ASCII.GetString(yarData, 0, bytes);
+        txtOutput.Invoke(UpdateTextboxDelegate, sRead);
+        
+        oTransmitPort.Write(sRead);
+        if(bLogging)
+          swWriter.WriteLine(sRead);
+      }
+    }
+
+    /// <summary>
+    /// Method to close the serial port
+    /// </summary>
+    private void CloseSerial()
+    {
+      try
+      {
+        oTransmitPort.Close();
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+    }
+
+    /// <summary>
+    /// Enables or disables the screen controls
+    /// </summary>
+    /// <param name="bRunning">Pass true if the read loop will be running</param>
+    private void EnableControls(bool bRunning)
+    {
+      btnStart.Enabled = !bRunning;      
+      chkLogging.Enabled = !bRunning;
+      cboSerialport.Enabled = !bRunning;
+      txtHost.Enabled = !bRunning;
+      txtPort.Enabled = !bRunning;
+
+      btnStop.Enabled = bRunning;
+    }
+
+    /// <summary>
+    /// Safely handle stopping the threads and closing network and serial resources
+    /// </summary>
+    private void Cleanup()
+    {
+      bTerminateTCPThread = true;
+      if (thTCPReadThread != null && thTCPReadThread.IsAlive)
+        thTCPReadThread.Join(500);
+      //thTCPReadThread.Abort();
+      if (oTransmitPort.IsOpen)
+      {
+        Thread thCloseSerial = new Thread(new ThreadStart(CloseSerial));
+        thCloseSerial.Start();
+      }
+      if (nsStream != null)
+        nsStream.Close();
+      if (tcpClient != null)
+        tcpClient.Close();
+      if (bLogging)
+        swWriter.Close();
+    }
+  }
+}
